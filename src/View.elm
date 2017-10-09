d65703d09a810e90da3fa428d14b1fa7fc789590
Mathew Bramson <mathewbramson@gmail.com>
2017-10-09 16:58:23 -0400

    Extract Header Views to Views.HeaderView

--- src/View.elm
+++ src/View.elm
@@ -1,5 +1,6 @@
 module View exposing (..)
 
+import Views.HeaderView exposing (navBar, flashMessage)
 import Html exposing (Html, Attribute, a, button, div, hr, h2, nav, p, text, input, span, ul, li)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (..)
@@ -32,71 +33,6 @@ page model =
 
         Models.NotFoundRoute ->
             notFoundView
-
-
-header : Model -> Html Msg
-header model =
-    div []
-        [ navBar model
-        , flashMessage model.flash
-        ]
-
-
-navBar : Model -> Html Msg
-navBar model =
-    nav [ class "navbar navbar-expand-md navbar-dark bg-dark" ]
-        [ a [ class "navbar-brand", href "#" ] [ text "HOME" ]
-        , navBarCollapseButton
-        , div [ class "collapse navbar-collapse", id "navbarSupportedContent" ]
-            [ ul [ class "navbar-nav mr-auto" ]
-                [ li [ class "nav-item active" ] [ currentUserNav model.currentUser ] ]
-            ]
-        ]
-
-
-navBarCollapseButton : Html Msg
-navBarCollapseButton =
-    button
-        [ class "navbar-toggler navbar-toggler-right"
-        , attribute "type" "button"
-        , attribute "data-toggle" "collapse"
-        , attribute "data-target" "#navbarSupportedContent"
-        , attribute "aria-controls" "navbarSupportedContent"
-        , attribute "aria-expanded" "false"
-        , attribute "aria-label" "Toggle navigation"
-        ]
-        [ span [ class "navbar-toggler-icon" ] [] ]
-
-
-currentUserNav : Maybe User -> Html Msg
-currentUserNav currentUser =
-    case currentUser of
-        Just user ->
-            a [ onClick Msgs.Logout, class "nav-link", href "#login" ] [ text "Log out" ]
-
-        Nothing ->
-            a [ class "nav-link", href "#login" ]
-                [ text "Log in" ]
-
-
-flashMessage : Maybe Flash -> Html Msg
-flashMessage flash =
-    case flash of
-        Just flash ->
-            flashAlert flash
-
-        Nothing ->
-            text ""
-
-
-flashAlert : Flash -> Html Msg
-flashAlert flash =
-    case flash.flash_type of
-        "error" ->
-            div [ class "alert alert-danger" ] [ text flash.message ]
-
-        other ->
-            div [ class "alert alert-primary" ] [ text flash.message ]
 
 
 homeView : Model -> Html Msg
