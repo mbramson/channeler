545907b3c0e62e56c43eda108304e040d932906e
Mathew Bramson <mathewbramson@gmail.com>
2017-10-08 20:39:19 -0400

    Use Bootstrap via CDN

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,8 @@ module.exports = {
   entry: {
     app: [
       './src/index.js',
-      './src/index.scss'
     ]
   },
-
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default'],
-      // In case you imported plugins individually, you must also require them here:
-      // Util: "exports-loader?Util!bootstrap/js/dist/util",
-      // Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-    })
-  ],
 
   output: {
     path: path.resolve(__dirname + '/dist'),
@@ -34,26 +21,6 @@ module.exports = {
           'style-loader',
           'css-loader',
         ]
-      },
-      {
-        test: /\.(scss)$/,
-        use: [{
-          loader: 'style-loader', // inject CSS to page
-        }, {
-          loader: 'css-loader', // translates CSS into CommonJS modules
-        }, {
-          loader: 'postcss-loader', // Run post css actions
-          options: {
-            plugins: function () { // post css plugins, can be exported to postcss.config.js
-              return [
-                require('precss'),
-                require('autoprefixer')
-              ];
-            }
-          }
-        }, {
-          loader: 'sass-loader' // compiles SASS to CSS
-        }]
       },
       {
         test:    /\.html$/,
